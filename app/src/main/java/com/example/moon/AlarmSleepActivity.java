a61db9a7febb68365fc5ea5572d8c8eee31a0dab
Chvykov Egor <chvykov.egor@yandex.ru>
2023-04-10 16:44:06 +0300

    egrt

--- app/src/main/java/com/example/moon/AlarmSleepActivity.java
+++ app/src/main/java/com/example/moon/AlarmSleepActivity.java
@@ -4,17 +4,35 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public class AlarmSleepActivity extends AppCompatActivity {
+
+    private Button recIntentButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_sleep);
+
+        recIntentButton = findViewById(R.id.recIntentButton);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.alpha);
+
+        recIntentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recIntentButton.startAnimation(animation);
+                startActivity(new Intent(AlarmSleepActivity.this, RecommendationActivity.class));
+            }
+        });
 
         createOneShotVibrationUsingVibrationEffect();
     }
@@ -24,4 +42,5 @@ public class AlarmSleepActivity extends AppCompatActivity {
         VibrationEffect effect = VibrationEffect.createOneShot(2000, VibrationEffect.DEFAULT_AMPLITUDE);
         vibrator.vibrate(effect);
     }
+
 }
